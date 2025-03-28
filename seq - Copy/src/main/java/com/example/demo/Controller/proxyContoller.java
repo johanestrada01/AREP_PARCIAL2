4482dadff9f38cd25001d981720fb4a2c3183364
unknown <johan.estrada-p@labinfo.is.escuelaing.edu.co>
2025-03-28 12:41:17 -0500

    java

---
+++ seq - Copy/src/main/java/com/example/demo/Controller/proxyContoller.java
@@ -0,0 +1,34 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/seq")
+public class proxyContoller {
+
+    String response;
+    HashMap<Integer, Integer> values = new HashMap<>();
+    @GetMapping("/{value}")
+    public String getValue(@PathVariable("value") int value) throws IOException {
+        values.put(0, 2);
+        values.put(1, 1);
+        calculateValue(value);
+        return values.values().toString();
+    }
+
+
+    public int calculateValue(int value){
+        if(!values.containsKey(value)){
+            values.put(value, calculateValue(value-1) + calculateValue(value-2));
+            return values.get(value);
+        }
+        else {
+            return values.get(value);
+        }
+    }
+
+}
